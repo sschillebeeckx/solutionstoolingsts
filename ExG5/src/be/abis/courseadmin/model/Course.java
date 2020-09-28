8b4d281b72de2d9460c575dcf03e92efe9aa423e
Sandy <sschillebeeckx@abis.be>
2020-09-28 17:28:34 +0200

    added G5

---
+++ ExG5/src/be/abis/courseadmin/model/Course.java
@@ -0,0 +1,105 @@
+package be.abis.courseadmin.model;
+
+import java.util.ArrayList;
+import java.io.BufferedWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.IOException;
+import java.util.Collections;
+
+public class Course {
+	
+	private String title;
+	private int numberOfDays;
+	private double pricePerDay;
+	private boolean priorKnowledgeRequired;
+	private ArrayList<Instructor> instructors = new ArrayList<>();
+	
+	public Course(String title,int numberOfDays,double pricePerDay){
+		this.title=title;
+		this.numberOfDays=numberOfDays;
+		this.pricePerDay=pricePerDay;
+	}
+	
+	public void setTitle(String title){
+		this.title=title;
+	}
+	
+	public String getTitle(){
+		return title;
+	}
+	
+	public void setNumberOfDays(int numberOfDays){
+		this.numberOfDays=numberOfDays;
+	}
+	
+	public int getNumberOfDays(){
+		return numberOfDays;
+	}
+	
+	public void setPricePerDay(double pricePerDay){
+		this.pricePerDay=pricePerDay;
+	}
+	
+	public double getPricePerDay(){
+		return pricePerDay;
+	}
+	
+	public void setPriorKnowledgeRequired(boolean priorKnowledgeRequired){
+		this.priorKnowledgeRequired=priorKnowledgeRequired;
+	}
+	
+	public boolean isPriorKnowledgeRequired(){
+		return priorKnowledgeRequired;
+	}
+	
+	public void setInstructors(ArrayList<Instructor> instructors){
+		this.instructors=instructors;
+	}
+	
+	public ArrayList<Instructor> getInstructors(){
+		return instructors;
+	}
+	
+	public void addInstructor(Instructor instructor){
+		instructors.add(instructor);
+	}
+	
+	public void removeInstructor(Instructor instructor){
+		instructors.remove(instructor);
+	}
+	
+	public void printInfo() throws IOException{
+	   BufferedWriter bw = Files.newBufferedWriter(Paths.get("printing.txt"));
+	   double totalPrice = this.calculatePrice();
+	   bw.write("The " + title + " course takes " + numberOfDays + " days and costs " + totalPrice + " euros. \nPrior knowledge required: " + priorKnowledgeRequired );
+	   String label;
+		if (totalPrice < 500) {
+			label = "CHEAP";
+		} else if (totalPrice >= 500 && totalPrice <=1500) {
+			label = "OK";
+		} else {
+			label = "EXPENSIVE";
+		}		
+	   bw.write("\nThat price is " + label);
+	   bw.write("\nThere are " + instructors.size() + " instructors for this course.");
+	   bw.write("\nThe instructors are:");
+	   Collections.sort(instructors);
+	   for (Instructor i: instructors){
+		   bw.write("\n"+i);
+	   }		
+	   bw.close();
+	   System.out.println("info is written to file");
+	}
+	
+	public double calculatePrice(){
+		double totalPrice = numberOfDays*pricePerDay;
+		if (!(numberOfDays>3 && priorKnowledgeRequired)) {
+			totalPrice*=1.21;
+		}
+		return totalPrice; 
+	}
+	
+	
+}
---
+++ ExG5/src/be/abis/courseadmin/test/ExG5.java
@@ -0,0 +1,37 @@
+package be.abis.courseadmin.test;
+
+import java.io.IOException;
+import java.time.LocalDate;
+
+import be.abis.courseadmin.exception.AgeCannotBeNegativeException;
+import be.abis.courseadmin.model.Course;
+import be.abis.courseadmin.model.Instructor;
+
+public class ExG5 {
+	
+	public static void main (String[] args) {
+		
+	  try {
+	    Instructor i1 = new Instructor("Sandy","Schillebeeckx",LocalDate.of(1978,4,10),23,2000.0);		
+		i1.printSalaryHistory("sandy.txt");
+		
+		Instructor i2 = new Instructor("Gie","Indesteege",LocalDate.of(1957,8,25),25,1900.0);		
+		i2.printSalaryHistory();
+		
+		Course c1 = new Course("Java",3,450.0);
+		c1.setPriorKnowledgeRequired(true);
+		c1.addInstructor(i1);
+		c1.addInstructor(i2);
+		
+		double totalPrice = c1.calculatePrice();
+				
+		c1.printInfo();
+		
+	  } catch (IOException| AgeCannotBeNegativeException  e){
+		  System.out.println(e.getMessage());
+	  } 
+	}
+	 
+	
+	
+}
